6e6fc85327786490a744b19f2b6a254d100d8f12
irah rosete <irah.rosete@outlook.com>
2023-01-08 00:03:46 +1100

    create serializer and view for drinks

---
+++ drinks/views.py
@@ -0,0 +1,8 @@
+from django.http import JsonResponse
+from .models import Drink
+from .serializers import DrinkSerialiser
+
+def drink_list(request):
+  drinks = Drink.objects.all()
+  serializer = DrinkSerialiser(drinks, many=True)
+  return JsonResponse(serializer.data)
