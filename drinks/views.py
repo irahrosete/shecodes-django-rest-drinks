e73ef0ddb254d67bfe72460844f5c96ef6920ad3
irah rosete <irah.rosete@outlook.com>
2023-01-08 00:54:13 +1100

    create post endpoint for drinks

--- drinks/views.py
+++ drinks/views.py
@@ -1,8 +1,19 @@
 from django.http import JsonResponse
 from .models import Drink
 from .serializers import DrinkSerialiser
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
+@api_view(['GET', 'POST'])
 def drink_list(request):
-  drinks = Drink.objects.all()
-  serializer = DrinkSerialiser(drinks, many=True)
-  return JsonResponse({'drinks': serializer.data})
+  if request.method == 'GET':
+    drinks = Drink.objects.all()
+    serializer = DrinkSerialiser(drinks, many=True)
+    return JsonResponse({'drinks': serializer.data})
+
+  if request.method == 'POST':
+    serializer = DrinkSerialiser(data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
